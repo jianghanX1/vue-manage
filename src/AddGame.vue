66456df5173d8ede6409521a83d47b28dadc326d
xjh <15200180653@163.com>
2023-04-24 10:03:28 +0800

    调整上传图片类型

--- src/AddGame.vue
+++ src/AddGame.vue
@@ -232,11 +232,11 @@ export default {
       })
     },
     beforeAvatarUpload(file) {
-      const isJPG = file.type === 'image/jpeg' || file.type === 'image/png' || file.type === 'image/jpg' || file.type === 'image/gif';
+      const isJPG = file.type === 'image/jpeg' || file.type === 'image/png' || file.type === 'image/jpg' || file.type === 'image/gif' || file.type === 'image/webp';
       const isLt2M = file.size / 1024 / 1024 < 2;
 
       if (!isJPG) {
-        this.$message.error('上传头像图片只能是 jpg/jpeg/gif/png 格式!');
+        this.$message.error('上传头像图片只能是 jpg/jpeg/gif/png/webp 格式!');
       }
       if (!isLt2M) {
         this.$message.error('上传头像图片大小不能超过 2MB!');
